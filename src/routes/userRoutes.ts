1186fc3686f8dc1ded0c677be5619315a4a15a91
mayowa-sys <mabdurrahman.balogun@gmail.com>
2025-09-08 22:44:20 +0100

    final toggle allow push notifications

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -6,7 +6,7 @@ const router = Router();
 router.put("/updatepictureurl", UserController.updatePictureUrl);
 router.put("/updatesubscription/:id", UserController.updateSubscription);
 router.get("/responserate", UserController.getResponseRate);
-router.post("/toggleallowpushnotifications", UserController.toggleAllowPushNotifications);
+router.post("/toggleallowpushnotifications/:id", UserController.toggleAllowPushNotifications);
 
 
 export default router;
