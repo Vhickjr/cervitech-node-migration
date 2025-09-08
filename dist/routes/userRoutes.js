cb048956132265553fc13cb5e2e47aa789c321ac
mayowa-sys <mabdurrahman.balogun@gmail.com>
2025-09-08 07:00:39 +0100

    update logger utility

--- dist/routes/userRoutes.js
+++ dist/routes/userRoutes.js
@@ -5,5 +5,5 @@ const user_controller_1 = require("../controllers/user.controller");
 const router = (0, express_1.Router)();
 router.put("/updatepictureurl", user_controller_1.UserController.updatePictureUrl);
 router.put("/updatesubscription/:id", user_controller_1.UserController.updateSubscription);
-router.get("get-response-rate/:id", user_controller_1.UserController.getResponseRate);
+router.get("/responserate", user_controller_1.UserController.getResponseRate);
 exports.default = router;
