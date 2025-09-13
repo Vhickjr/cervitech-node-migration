2eab90265524cb8f8c632cd84a6168f8380a4048
mayowa-sys <mabdurrahman.balogun@gmail.com>
2025-09-13 19:35:19 +0100

    Remove dist folder from repo and add to gitignore

--- dist/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const user_controller_1 = require("../controllers/user.controller");
-const router = (0, express_1.Router)();
-router.put("/updatepictureurl", user_controller_1.UserController.updatePictureUrl);
-router.put("/updatesubscription/:id", user_controller_1.UserController.updateSubscription);
-router.get("/responserate", user_controller_1.UserController.getResponseRate);
-exports.default = router;
