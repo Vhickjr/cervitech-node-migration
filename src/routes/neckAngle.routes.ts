d0a91319ee20ced32e820a9cba593a1297cc0c8c
mayowa-sys <mabdurrahman.balogun@gmail.com>
2025-09-15 12:10:53 +0100

    finishing touches

--- src/routes/neckAngle.routes.ts
+++ src/routes/neckAngle.routes.ts
@@ -6,6 +6,5 @@ const router = express.Router();
 
 router.post('/postBatchNeckAngleRecords', NeckAngleController.postBatchNeckAngleRecords);
 router.post('/postrandomneckanglerecords', NeckAngleController.postRandomTestBatchNeckAngleRecords);
-router.post("/weekly-averages", NeckAngleController.getWeeklyNeckAngleAverages);
 
 export default router;
