ebf977c44fdee4d7ef68902fdee5f7f9980d1c8a
adedamola-toye <toye.adedamola@gmail.com>
2025-09-16 21:23:59 +0100

    Updates in the ompute neck angle parameters

---
+++ src/viewmodels/NeckAngleParameters.viewmodel.ts
@@ -0,0 +1,20 @@
+import { AbbreviatedNeckAngleRecordViewModel } from './AbbreviatedNeckAngleRecord.viewmodel';
+import { DailyAngleDataViewModel } from './DailyAngleData.viewmodel';
+import { GoalCycleReportViewModel } from './GoalCycleReport.viewmodel';
+
+export interface NeckAngleParametersViewModel {
+  username: string;
+  averageNeckAngleStarRatingOver5: number;
+  responseRate: number;
+  lastSetGoalWithDetails?: GoalCycleReportViewModel | null;
+  dailyNeckAngleRecords: AbbreviatedNeckAngleRecordViewModel[];
+  weeklyNeckAngleRecords: AbbreviatedNeckAngleRecordViewModel[];
+  monthlyNeckAngleRecords: AbbreviatedNeckAngleRecordViewModel[];
+  currentDayAverageNeckAngle: number;
+  currentWeekAverageNeckAngle: number;
+  currentMonthAverageNeckAngle: number;
+  bestWeekDayAverageNeckAngle?: DailyAngleDataViewModel | null;
+  badWeekDayAverageNeckAngle?: DailyAngleDataViewModel | null;
+  averageNeckAngleForEachDayOfTheCurrentWeek: DailyAngleDataViewModel[];
+  //averageNeckAngleForEachWeekOfTheCurrentMonth: { weekNumber: number; averageNeckAngle: number }[];
+}
