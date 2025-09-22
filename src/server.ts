25820e5cad3f9af549610b0e239633fdb502d087
Chiemezie Jacob <Chiemezie.jacob@gmail.com>
2025-09-22 10:57:59 +0100

    Cleaned up server.ts file

--- src/server.ts
+++ src/server.ts
@@ -19,7 +19,6 @@ const app = express();
 app.use(bodyParser.json())
 const PORT = process.env.PORT || 4000;
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/cervitechdb';
-console.log(MONGODB_URI);
 
 // Middleware
 app.use(express.json());
