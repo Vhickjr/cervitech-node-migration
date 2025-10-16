6cf4c827325e31cd791b542ce406b2f6298e74a1
adedamola-toye <toye.adedamola@gmail.com>
2025-10-16 17:05:40 +0100

    replace manual validation check with joi schemas

--- src/validation/authValidation.ts
+++ src/validation/authValidation.ts
@@ -1,54 +1,41 @@
 import { LoginRequest, LogoutRequest } from '../types/auth.types';
 import { SignupRequest } from '../viewmodels/auth.viewmodel';
-import { MOBILE_CHANNEL } from '../enums/mobileChannel';
+import {signupSchema, loginSchema, logoutSchema} from './schemas/auth.schema'
 
 export class AuthValidation {
   static signupValidation(input: SignupRequest): string | null {
-    if (!input.email) {
-      return 'Email is required';
-    }
-    if (!input.password) {
-      return 'Password is required';
-    }
-    if (!input.lastName) {
-      return 'Last name is required';
-    }
-    if (!input.firstName) {
-      return 'First name is  required';
-    }
-    if (!input.username) {
-      return 'Username is  required';
-    }
-    if (input.mobileChannel === undefined || input.mobileChannel === null) {
-      return 'Mobile channel is required';
-    }
+    const {error} = signupSchema.validate(input, {
+      abortEarly: true,
+      allowUnknown:false
+    });
 
-    return null;
+    if(error){
+      return error.details[0].message.replace(/['"]+/g, '')
+    }
+    return null
   }
 
   static loginValidation(input: LoginRequest): string | null {
-    if (!input.emailOrUsername) {
-      return 'Email or username is required';
+    const {error} = loginSchema.validate(input, {
+      abortEarly: true,
+      allowUnknown:false
+    });
+
+    if(error){
+      return error.details[0].message.replace(/['"]+/g, '')
     }
-    if (!input.password) {
-      return 'Password is required';
-    }
-    if (input.mobileChannel === undefined || input.mobileChannel === null) {
-      return 'Mobile channel is required';
-    }
-    if (!Object.values(MOBILE_CHANNEL).includes(input.mobileChannel)) {
-      return 'Invalid mobile channel provided';
-    }
-    return null;
+    return null
   }
 
   static logoutValidation(input: LogoutRequest): string | null {
-    if (!input.userId) {
-      return 'User id is required';
+    const {error} = logoutSchema.validate(input, {
+      abortEarly: true,
+      allowUnknown:false
+    });
+
+    if(error){
+      return error.details[0].message.replace(/['"]+/g, '')
     }
-    if (!input.token) {
-      return 'Token is required';
-    }
-    return null;
+    return null
   }
 }
