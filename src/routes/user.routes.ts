3f17f5627318359f6722318e983301c75a98fb27
Oseni03 <oseniayomide57@gmail.com>
2026-05-19 22:47:40 +0100

    fix: make sure /api/v1/user/updatepictureurl is an authenticated endpoint

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -6,17 +6,17 @@ import { authenticateJWT } from "../middlewares/auth.middleware.js";
 const router = Router();
 
 
-router.put("/updatepictureurl", UserController.updatePictureUrl);
+router.put("/updatepictureurl", authenticateJWT, UserController.updatePictureUrl);
 router.put("/updatesubscription/:id", UserController.updateSubscription);
 
 // router.post("/postresponserate", UserController.postResponseRate);
 router.get("/getresponserate", UserController.getResponseRate);
-router.post('/logout', authenticateJWT,AuthController.logout);
+router.post('/logout', authenticateJWT, AuthController.logout);
 router.get("/usernameAlreadyExists", AuthController.usernameAlreadyExists);
 router.get("/isValidEmail", AuthController.isValidEmail);
 router.post("/toggleallowpushnotifications/:id", UserController.toggleAllowPushNotifications);
 router.delete("/deleteaccount/:id", UserController.deleteById);
-router.delete("/deletemyaccount", UserController.deleteMyAccount); 
+router.delete("/deletemyaccount", UserController.deleteMyAccount);
 router.get("/confirmdeletemyaccount", UserController.confirmDeleteMyAccount);
 router.delete("/deleteall", UserController.deleteAll);
 router.get("/getByEmail", UserController.getByEmail);
